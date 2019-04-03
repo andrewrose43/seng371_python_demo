4546511d9a42ce3a01382023fb8ddbcf874cb347
Andrew Rose <andrew.rose43@gmail.com>
2019-04-03 13:01:19 -0700

    Looping complete; time to filter

--- stac_demo.py
+++ stac_demo.py
@@ -1,33 +1,32 @@
 import urllib.request
 import json
 
+# The URL for the MUX camera
 baseURL = 'https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/'
 
 # Trawl that catalog!
 def main():
 
-	# Let's test that we can get one JSON object...
-	# test_json = urllib.request.urlopen('https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/057/095/CBERS_4_MUX_20171121_057_095_L2.json').read()
-	# print("\n" + str(test_json) + "\n")
-
-
 	# Get the MUX catalog object as a Python dictionary
-	mux_json_obj = json.loads(urllib.request.urlopen('https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/catalog.json').read())
+	mux_json_obj = json.loads(urllib.request.urlopen(baseURL + 'catalog.json').read())
 	for path_link in mux_json_obj["links"]:
 		if path_link['rel'] == 'child':
-			# print('https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/' + path_link['href'])
-			path_json_obj = json.loads(urllib.request.urlopen('https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/' + path_link['href']).read())
-			# print(path_json_obj)
-			# print(path_link['href'][:3])
-			# print(path_link)
+			# Get each path's catalog object as a Python dictionary
+			path_json_obj = json.loads(urllib.request.urlopen(baseURL + path_link['href']).read())
 			for row_link in path_json_obj["links"]:
 				if row_link['rel'] == 'child':
-					# print(row_link)
+					# Get each row's catalog object as a Python dictionary
+					# note that the URL has the path's three digits and '/' added in
+					row_json_obj = json.loads(urllib.request.urlopen(baseURL + path_link['href'][:4] + row_link['href']).read())
+					# Finally, we loop through the actual items
+					for item_link in row_json_obj["links"]:
+						if item_link['rel'] == 'item':
+							# print(item_link)
+							# Get each item as a Python dictionary
+							item_obj = json.loads(urllib.request.urlopen(baseURL + path_link['href'][:4] + row_link['href'][:4] + item_link['href']).read())
+							# print(item_obj)
 
-					# Below, we see the ROW JSON with all its items. Yay
-					row_json_obj = json.loads(urllib.request.urlopen('https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/' + path_link['href'][:4] + row_link['href']).read())
-					# print(str(json.loads(urllib.request.urlopen('https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/' + path_link['href'][:4] + row_link['href']).read())))
-					# print('https://cbers-stac-0-6.s3.amazonaws.com/CBERS4/MUX/' + path_link['href'][:4] + row_link['href'])
+							# Okay. Here, we need to check each item's bounding box and timestamp to figure out if we want it
 
 if __name__ == "__main__":
 	main()
